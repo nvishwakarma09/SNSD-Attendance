be344c47233080594a2f8e9ec4e0fe74e9adf604
Nitin Vishwakarma <nitinbishwakarma@gmail.com>
2026-08-27 01:46:42 +0530

    added reset db code

--- config/setting.py
+++ config/setting.py
@@ -7,6 +7,7 @@ class Settings:
         "sqlite:///./attendance.db"
     )
     SECRET_KEY: str = os.getenv("SECRET_KEY", "snm.attendance.123")
+    DATABASE_RESET_ENABLED: bool = os.getenv("DATABASE_RESET_ENABLED", "false").lower() == "true"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 30
